cf5c04dd6bc2f010fb09633cbf8aa5d742405fa8
JosephAr1 <jonielhet@yahoo.com>
2021-06-20 11:37:46 +1200

    Add project files.

---
+++ Week3_Q3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Week3_Q3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] num = new int[] { 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            Console.WriteLine();
+            Console.WriteLine("Arrays in Proper order Order:");
+            
+            foreach (int item in num)
+            {             
+                Console.Write(item);            
+            }
+            {
+                Console.WriteLine();
+                Console.WriteLine("Arrays in Reverse Order:");
+                
+            }           
+            Array.Reverse(num);
+            foreach(int value in num)
+            {
+                Console.Write(value);
+            }
+                
+        }
+    }
+}
